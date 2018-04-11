ac1a01ad6ddeff0115708b9e17995382713b12c4
Michael R Sweet <michael.r.sweet@gmail.com>
2018-04-11 15:21:08 -0400

    Update with authentication fix from CUPS upstream.
    Add script for tracking CUPS upstream changes.

---
+++ scripts/update-cups.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Script to merge changes from CUPS 2.2.x upstream into the CUPS directory.
+#
+
+if test ! -x scripts/update-cups.sh; then
+	echo Must run this script from the top directory.
+	exit 1
+fi
+
+if test ! -d ../cups-2.2; then
+	echo Have CUPS 2.2.x checked out in ../cups-2.2.
+	exit 1
+fi
+
+oldrev=`cat .cups-upstream`
+newrev=`cd ../cups-2.2; git show | head -1 | awk '{print $2}'`
+
+(cd ../cups-2.2; git diff $oldrev cups ':!cups/Dependencies' ':!cups/Makefile' ':!cups/libcups2.def' ':!cups/ppd*' ':!cups/test*') >$newrev.patch
+git apply $newrev.patch && (echo $newrev >.cups-upstream)
