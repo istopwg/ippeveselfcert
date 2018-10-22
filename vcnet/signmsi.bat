d2b5ff73b81ed2bb5fea2c46213be51be34112f3
Michael R Sweet <michael.r.sweet@gmail.com>
2018-10-22 09:37:57 -0400

    Fix some more VS project issues.

--- vcnet/signmsi.bat
+++ vcnet/signmsi.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Windows Kits\8.1\bin\x86\signtool" sign /n "IEEE Industry Standards and Technology Organization" sw-ippeveselfcert10-windows.msi
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /n "IEEE Industry Standards and Technology Organization" ..\sw-ippeveselfcert10-YYYYMMDD-windows.msi
 
