a7c16b5f7af9aa621198037935a2fcab5454e340
Michael R Sweet <michael.r.sweet@gmail.com>
2020-02-10 15:52:25 -0500

    Bump version.

--- vcnet/config.h
+++ vcnet/config.h
@@ -99,8 +99,8 @@ typedef unsigned long useconds_t;
  * Version of software...
  */
 
-#define CUPS_SVERSION "IPPEVESELFCERT11 v20190425"
-#define CUPS_MINIMAL "IPPEVESELFCERT11/20190425"
+#define CUPS_SVERSION "IPPEVESELFCERT11 v20200210"
+#define CUPS_MINIMAL "IPPEVESELFCERT11/20200210"
 
 
 /*
