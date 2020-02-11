688299b97ac4cd57408af69722591760493e92de
Michael R Sweet <michael.r.sweet@gmail.com>
2020-02-10 19:40:40 -0500

    Bump version/copyright/license info.

--- tests/dnssd-tests.bat
+++ tests/dnssd-tests.bat
@@ -1,18 +1,11 @@
 @echo off
 ::
-:: IPP Everywhere Printer Self-Certification Manual 1.0: Section 5: DNS-SD Tests.
+:: IPP Everywhere Printer Self-Certification Manual 1.1: Section 5: DNS-SD Tests.
 ::
-:: Copyright 2014-2018 by The Printer Working Group.
+:: Copyright 2014-2020 by the IEEE-ISTO Printer Working Group.
 ::
-:: This program may be copied and furnished to others, and derivative works
-:: that comment on, or otherwise explain it or assist in its implementation may
-:: be prepared, copied, published and distributed, in whole or in part, without
-:: restriction of any kind, provided that the above copyright notice and this
-:: paragraph are included on all such copies and derivative works.
-::
-:: The IEEE-ISTO and the Printer Working Group DISCLAIM ANY AND ALL WARRANTIES,
-:: WHETHER EXPRESS OR IMPLIED INCLUDING (WITHOUT LIMITATION) ANY IMPLIED
-:: WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+:: Licensed under Apache License v2.0.  See the file "LICENSE" for more
+:: information.
 ::
 :: Usage:
 ::
