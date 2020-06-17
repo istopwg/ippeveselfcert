899423f470605dd5a0caae5a20bdecc608af25bb
Michael R Sweet <michael.r.sweet@gmail.com>
2020-06-17 08:39:00 -0400

    Prep for update 1.

--- vcnet/config.h
+++ vcnet/config.h
@@ -99,8 +99,8 @@ typedef unsigned long useconds_t;
  * Version of software...
  */
 
-#define CUPS_SVERSION "IPPEVESELFCERT11 v20200521"
-#define CUPS_MINIMAL "IPPEVESELFCERT11/20200521"
+#define CUPS_SVERSION "IPPEVESELFCERT11 v20200617"
+#define CUPS_MINIMAL "IPPEVESELFCERT11/20200617"
 
 
 /*
