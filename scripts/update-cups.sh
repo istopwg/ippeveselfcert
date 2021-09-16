b34cc625fe553129c3bde0008a5960657092d0fd
Michael R Sweet <michael.r.sweet@gmail.com>
2021-09-16 11:51:36 -0400

    Merge CUPS master@ecc5afc75839eb73481a2ec41fbf7762c5ee7ea2 from upstream.

--- scripts/update-cups.sh
+++ scripts/update-cups.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Script to merge changes from CUPS 2.3.x upstream into the CUPS directory.
+# Script to merge changes from CUPS upstream into the CUPS directory.
 #
 
 if test ! -x scripts/update-cups.sh; then
@@ -16,5 +16,5 @@ fi
 oldrev=`cat .cups-upstream`
 newrev=`cd ../cups; git show | head -1 | awk '{print $2}'`
 
-(cd ../cups; git diff $oldrev cups man/ipp*.[15] tools ':!*/Dependencies' ':!*/Makefile' ':!cups/libcups2.def' ':!cups/adminutil*' ':!cups/back*' ':!cups/getdevices*' ':!cups/getifaddrs*' ':!cups/interpret*' ':!cups/ppd*' ':!cups/raster-inter*' ':!cups/sidechannel*' ':!cups/test*' ':!cups/snmp*' ':!cups/tlscheck.c' ':!cups/cupspm*' ':!cups/*.shtml' ':!cups/*.header' ':!tools/ippevepcl.c' ':!tools/ippeveps.c') >$newrev.patch
+(cd ../cups; git diff $oldrev cups man/ipp*.[15] tools ':!*/Dependencies' ':!*/Makefile' ':!cups/libcups2.def' ':!cups/adminutil*' ':!cups/back*' ':!cups/fuzzipp.c' ':!cups/getdevices*' ':!cups/getifaddrs*' ':!cups/interpret*' ':!cups/ppd*' ':!cups/raster-inter*' ':!cups/sidechannel*' ':!cups/test*' ':!cups/snmp*' ':!cups/tlscheck.c' ':!cups/cupspm*' ':!cups/*.shtml' ':!cups/*.header' ':!tools/ippevepcl.c' ':!tools/ippeveps.c') >$newrev.patch
 git apply $newrev.patch && (echo $newrev >.cups-upstream; git commit -a -e -m "Merge changes from CUPS master@$newrev"; rm -f $newrev.patch) || echo "$newrev.patch did not apply."
