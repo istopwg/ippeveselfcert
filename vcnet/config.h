b6c2de7f9f43ff3c9911d80755659a674413064b
Michael R Sweet <michael.r.sweet@gmail.com>
2022-04-28 16:31:40 -0400

    Reduce document tests to 4 pages for the A4/Letter tests, as before.
    Bump version (tomorrow's date to give me time to make some other changes...)

--- vcnet/config.h
+++ vcnet/config.h
@@ -99,8 +99,8 @@ typedef unsigned long useconds_t;
  * Version of software...
  */
 
-#define CUPS_SVERSION "IPPEVESELFCERT11 v20220414"
-#define CUPS_MINIMAL "IPPEVESELFCERT11/20220414"
+#define CUPS_SVERSION "IPPEVESELFCERT11 v20220429"
+#define CUPS_MINIMAL "IPPEVESELFCERT11/20220429"
 
 
 /*
