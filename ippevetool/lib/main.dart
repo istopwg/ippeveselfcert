f178053d04091060f7826be203e0cde1c62dec53
Michael R Sweet <michael.r.sweet@gmail.com>
2022-11-13 11:23:28 -0500

    Add placeholder details page when clicking on a printer.

--- ippevetool/lib/main.dart
+++ ippevetool/lib/main.dart
@@ -36,17 +36,9 @@ class IppEveToolApp extends StatelessWidget {
   }
 }
 
+// Home page with list of printers...
 class IppEveHomePage extends StatefulWidget {
   const IppEveHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -118,5 +110,31 @@ class _IppEveHomePageState extends State<IppEveHomePage> {
 
   _onPrinterTap(BuildContext context, NsdServiceInfo printer) {
     print(printer.name);
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => IppEveDetailsPage(printer: printer,)),
+    );
+  }
+}
+
+// Details page for printers...
+class IppEveDetailsPage extends StatefulWidget {
+  const IppEveDetailsPage({super.key, required this.printer});
+
+  final NsdServiceInfo printer;
+
+  @override
+  State<IppEveDetailsPage> createState() => _IppEveDetailsPageState();
+}
+
+class _IppEveDetailsPageState extends State<IppEveDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.printer.name ?? "Unknown"),
+      ),
+      body: const Center(child: Text("Details")),
+    );
   }
 }
