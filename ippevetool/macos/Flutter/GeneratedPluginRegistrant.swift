87e948151704137e95824b02723f39fc46556804
Michael R Sweet <msweet@msweet.org>
2026-07-27 17:11:27 -0400

    Save generated flutter code.

--- ippevetool/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ippevetool/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import nsd_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   NsdMacosPlugin.register(with: registry.registrar(forPlugin: "NsdMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
